affb9caf61a0c8b4b183ad179c1564fefa406f47
Dima Snisarenko <snisarenkodima@gmail.com>
2021-11-09 13:18:22 +0100

    chore: init project with yarn workspaces

---
+++ packages/extension-content-ui/stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'extension-content-ui',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
