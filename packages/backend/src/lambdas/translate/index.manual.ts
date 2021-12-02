c3597be4890f9b8a70d2666b5d10a78385ad4cff
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-02 10:56:41 +0100

    feat(backend): translate text using google translate API

--- packages/backend/src/lambdas/translate/index.manual.ts
+++ packages/backend/src/lambdas/translate/index.manual.ts
@@ -12,27 +12,11 @@ describe('integration check for translate lambda', () => {
   }
 
   it('execute translate lambda', async () => {
+    mockEvent.body = JSON.stringify({
+      phrase: 'dankjewel',
+    });
     const result = await translate(mockEvent);
     console.log({ result });
     expect(result.statusCode).toEqual(200);
-  });
-
-  it('tests translation client', async () => {
-    const translationClient = new TranslationServiceClient();
-
-    const projectId = 'vocably-332109';
-    const location = 'global';
-    const text = 'bijgelegd';
-
-    const request = {
-      parent: `projects/${projectId}/locations/${location}`,
-      contents: [text],
-      mimeType: 'text/plain', // mime types: text/plain, text/html
-      targetLanguageCode: 'en',
-    };
-
-    // Run request
-    const [response] = await translationClient.translateText(request);
-    console.log(response);
   });
 });
--- packages/backend/src/lambdas/translate/index.ts
+++ packages/backend/src/lambdas/translate/index.ts
@@ -1,15 +1,16 @@
-import { lastValueFrom, of } from 'rxjs';
+import { lastValueFrom, mergeMap, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { buildResponse } from '../../utils/buildResponse';
 import { buildErrorResponse } from '../../utils/buildErrorResponse';
+import { translateText } from './translateText';
 
 export const translate = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> =>
   lastValueFrom(
     of(event).pipe(
-      map((event) => ({ time: new Date().toISOString(), event })),
+      mergeMap(translateText),
       map((data) =>
         buildResponse({
           body: JSON.stringify(data),
