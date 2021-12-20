9027c84a37b6a4bce82c64e3686f7d6d982351e0
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-20 13:44:44 +0100

    feat(www): post email

--- packages/www/index.js
+++ packages/www/index.js
@@ -1,9 +1,16 @@
 window.addEventListener('load', () => {
   const form = document.getElementById('form');
   const success = document.getElementById('success');
+  const email = document.getElementById('email');
+
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     form.classList.add('hidden');
     success.classList.add('visible');
+
+    fetch('https://backend.vocably.pro/email', {
+      method: 'POST',
+      body: JSON.stringify({ email: email.value }),
+    });
   });
 });
