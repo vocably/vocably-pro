2b5b78b7495f0adf37cc5669d9e840dbac99ee9d
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-27 15:07:29 +0100

    chore: remove redundant comments

--- platform/variables.tf
+++ platform/variables.tf
@@ -21,31 +21,3 @@ variable "lexicala_username" {
 variable "lexicala_password" {
   type = string
 }
-
-#variable "auth_domain" {
-#  type    = string
-#  default = "auth.vocably.pro"
-#}
-#
-#variable "gcloud_project" {
-#  type    = string
-#  default = "vocably-332109"
-#}
-#
-#variable "lexicala_username" {
-#  type = string
-#}
-#
-#variable "lexicala_password" {
-#  type = string
-#}
-#
-#variable "google_oauth2_client_id" {
-#  type    = string
-#  default = "356478205648-le03fnat0mcc5ro8u9d7bphr5gsvgveb.apps.googleusercontent.com"
-#}
-#
-#variable "user_pool_endpoint" {
-#  type    = string
-#  default = "cognito-idp.eu-central-1.amazonaws.com/eu-central-1_vWIKfxc8A"
-#}
