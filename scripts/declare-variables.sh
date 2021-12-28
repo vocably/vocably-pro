c3503850186c836240b9f94c59960b10d80a7c57
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-28 15:10:24 +0100

    debug

---
+++ scripts/declare-variables.sh
@@ -0,0 +1,7 @@
+declare -a variables=("LEXICALA_USERNAME" "LEXICALA_PASSWORD")
+
+for variable in "${variables[@]}"
+do
+   envVar="${ENV_NAME}_${variable}"
+   echo "export ${variable}=${!envVar}" >> $BASH_ENV
+done
