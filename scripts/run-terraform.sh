db679b65887791f462562963de368aa5c50b0064
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-29 10:50:54 +0100

    debug

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -3,6 +3,9 @@
 echo $GCP_KEY >> google.json
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/google.json"
 
+echo $GOOGLE_APPLICATION_CREDENTIALS
+cat google.json
+
 cd ./platform
 terraform init -backend-config=./${ENV_NAME}.remote
 terraform workspace select stage
