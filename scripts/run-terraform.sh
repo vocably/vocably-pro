20a684752890aaa242ffac8f8c68377d815e72f9
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-29 13:19:19 +0100

    debug

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -4,3 +4,4 @@ cd ./platform
 terraform init -backend-config=./${env_name}.remote
 terraform workspace select ${env_name}
 terraform plan -var-file="${env_name}.tfvars"
+terraform apply -var-file="${env_name}.tfvars" -auto-approve
