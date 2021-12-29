3ff4a49825207dd9f91eb2ef74d4123b8915af41
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-29 10:55:16 +0100

    debug

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-echo $GCP_KEY >> google.json
+echo $STAGE_GCP_KEY >> google.json
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/google.json"
-
-echo $GOOGLE_APPLICATION_CREDENTIALS
-cat google.json
-echo $GCP_KEY
-echo $STAGE_GCP_KEY
-echo $STAGE_GOOGLE_CREDENTIALS
 
 cd ./platform
 terraform init -backend-config=./${ENV_NAME}.remote
