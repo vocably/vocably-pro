f5582b5407e05ac6926f4fc7f8cc92c8475491c0
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-30 18:52:39 +0100

    ci: install zip and aws-cli dependencies

--- scripts/install-build-depdendencies.sh
+++ scripts/install-build-depdendencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y unzip
+sudo apt-get install -y unzip zip aws-cli
 wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip
 unzip terraform_1.1.2_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
