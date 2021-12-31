0b4a5c86d6caf72907bcfa703d2e96f81c44f0eb
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-31 12:11:12 +0100

    chore: properly install aws cli

--- scripts/install-build-depdendencies.sh
+++ scripts/install-build-depdendencies.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y unzip zip aws-cli
+sudo apt-get install -y unzip zip
+
 wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip
 unzip terraform_1.1.2_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
