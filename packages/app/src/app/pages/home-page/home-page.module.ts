ac365907e4fef2538dad75005ee1927d00305ecc
Dima Snisarenko <snisarenkodima@gmail.com>
2022-01-05 14:36:10 +0100

    feat(app): no cards message with the optional install cta

--- packages/app/src/app/pages/home-page/home-page.module.ts
+++ packages/app/src/app/pages/home-page/home-page.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
+import { NoCardsComponent } from './no-cards/no-cards.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [HomePageComponent],
-  imports: [CommonModule],
+  declarations: [HomePageComponent, NoCardsComponent],
+  imports: [CommonModule, HttpClientModule],
 })
 export class HomePageModule {}
