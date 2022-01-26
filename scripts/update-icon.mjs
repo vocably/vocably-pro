f7296d7f1025a7204e97fb6596f493eda0645496
Dima Snisarenko <snisarenkodima@gmail.com>
2022-01-26 16:08:49 +0100

    chore: update icon

---
+++ scripts/update-icon.mjs
@@ -0,0 +1,32 @@
+import 'zx/globals';
+import { rootDir } from './helpers/dirs.mjs';
+import { readFileSync, writeFileSync } from 'fs';
+
+const svgFilename = 'icon.svg';
+const icoFilename = 'favicon.ico';
+const devFaviconFilename = 'dev-favicon.ico';
+
+cd(rootDir);
+await $`convert assets/${svgFilename} -set colorspace Gray -separate -average assets/${devFaviconFilename}`;
+await $`convert assets/${svgFilename} assets/${icoFilename}`;
+await $`convert -density 1200 assets/${svgFilename} assets/icon.png`;
+
+for (let size of ['16', '32', '48', '128']) {
+  const iconFileName = `icon-${size}x${size}.png`;
+  await $`svgexport assets/${svgFilename} packages/extension/assets/images/${iconFileName} ${size}:${size}`;
+}
+
+await $`cp assets/${devFaviconFilename} packages/extension-content-script/public/favicon.ico`;
+await $`cp assets/${devFaviconFilename} packages/app/src/favicon.ico`;
+await $`cp assets/${icoFilename} packages/www/favicon.ico`;
+
+const svgImage = readFileSync(`assets/${svgFilename}`, 'utf8').toString();
+const scssTemplate = readFileSync(`assets/_icon.scss.tpl`, 'utf8').toString();
+writeFileSync(
+  'assets/_icon.scss',
+  scssTemplate.replace(
+    '%DATA_IMAGE%',
+    `data:image/svg+xml;base64,${Buffer.from(svgImage).toString('base64')}`
+  )
+);
+console.log('Button icon has been successfully updated.');
