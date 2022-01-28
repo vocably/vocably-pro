6f92203780f42d11c441be6d10c79987f8c51f20
Dima Snisarenko <snisarenkodima@gmail.com>
2022-01-28 11:34:06 +0100

    fix(app): redirect to the first deck if possible

--- packages/app/src/app/decks/decks.component.ts
+++ packages/app/src/app/decks/decks.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { from, Subject, takeUntil, withLatestFrom } from 'rxjs';
+import { from, Subject, takeUntil, tap } from 'rxjs';
 import { DeckListStoreService } from './deck-list-store.service';
 import { listLanguages } from '@vocably/api';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'vocably-decks',
@@ -13,30 +12,19 @@ export class DecksComponent implements OnInit, OnDestroy {
 
   isLoading = true;
 
-  constructor(
-    decksListStore: DeckListStoreService,
-    router: Router,
-    route: ActivatedRoute
-  ) {
-    if (route.firstChild === null) {
-      return;
-    }
-
+  constructor(decksListStore: DeckListStoreService) {
     from(listLanguages())
-      .pipe(takeUntil(this.destroy$), withLatestFrom(route.firstChild.params))
-      .subscribe(([result, params]) => {
+      .pipe(
+        takeUntil(this.destroy$),
+        tap(() => (this.isLoading = true))
+      )
+      .subscribe((result) => {
         if (result.success === false) {
           throw new Error(result.reason);
         }
 
         this.isLoading = false;
         decksListStore.store(result.value);
-
-        if (params['language'] === undefined) {
-          router.navigate([result.value[0]], {
-            relativeTo: route,
-          });
-        }
       });
   }
 
--- packages/app/src/app/decks/pages/no-decks-page/no-decks-page.component.ts
+++ packages/app/src/app/decks/pages/no-decks-page/no-decks-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DeckListStoreService } from '../../deck-list-store.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'vocably-no-decks-page',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-decks-page.component.scss'],
 })
 export class NoDecksPageComponent implements OnInit {
-  constructor() {}
+  constructor(
+    deckListStore: DeckListStoreService,
+    router: Router,
+    route: ActivatedRoute
+  ) {
+    if (deckListStore.decks$.value.length > 0) {
+      router.navigate([deckListStore.decks$.value[0]], {
+        relativeTo: route,
+      });
+    }
+  }
 
   ngOnInit(): void {}
 }
