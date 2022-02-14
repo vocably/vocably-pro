651ca42dc43c34644c3db0983f7a8744d4d02824
Dima Snisarenko <snisarenkodima@gmail.com>
2022-02-14 13:54:34 +0100

    chore(platform-root): shift to aws 4.0 provider

--- platform-root/tf-state/main.tf
+++ platform-root/tf-state/main.tf
@@ -9,16 +9,27 @@ terraform {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "vocably-${var.environment}-tf-state"
-  acl    = "private"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_acl" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
