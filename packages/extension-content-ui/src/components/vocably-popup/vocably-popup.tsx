8e307bc43c032ea09596ff8f5d40648a5980b6a1
Dima Snisarenko <snisarenkodima@gmail.com>
2022-02-16 13:46:27 +0100

    refactor(extension): popup height could be restricted from the outside

--- packages/extension-content-ui/src/components/vocably-popup/vocably-popup.tsx
+++ packages/extension-content-ui/src/components/vocably-popup/vocably-popup.tsx
@@ -1,4 +1,11 @@
-import { Component, Host, h, Event, EventEmitter } from '@stencil/core';
+import {
+  Component,
+  Host,
+  h,
+  Event,
+  Element,
+  EventEmitter,
+} from '@stencil/core';
 
 @Component({
   tag: 'vocably-popup',
@@ -6,18 +13,38 @@ import { Component, Host, h, Event, EventEmitter } from '@stencil/core';
   shadow: true,
 })
 export class VocablyPopup {
+  @Element() el: HTMLElement;
   @Event() close: EventEmitter<void>;
+
+  componentDidRender() {
+    const contentWrapper = this.el.shadowRoot.getElementById('content-wrapper');
+    const content = this.el.shadowRoot.getElementById('content');
+    const resizeObserver = new ResizeObserver(() => {
+      const rect = content.getBoundingClientRect();
+      contentWrapper.style.width = `${rect.width}px`;
+      contentWrapper.style.height = `${rect.height}px`;
+    });
+
+    resizeObserver.observe(content);
+  }
+
   render() {
     return (
       <Host>
         <div class="popup">
-          <div class="caption">
+          <div class="body" id="body">
+            <div class="content-wrapper" id="content-wrapper">
+              <div class="content-unlimited">
+                <div class="content" id="content">
+                  <slot></slot>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="header">
             <button class="close" onClick={() => this.close.emit()}>
               &times;
             </button>
-          </div>
-          <div class="body">
-            <slot></slot>
           </div>
         </div>
       </Host>
