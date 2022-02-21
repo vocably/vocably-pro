a5c204d88816f9b9c556748b120d062699f2bd81
Dima Snisarenko <snisarenkodima@gmail.com>
2022-02-21 16:52:59 +0100

    fix: path_part analyze -> translate

--- platform/api-analyze.tf
+++ platform/api-analyze.tf
@@ -23,7 +23,7 @@ resource "aws_cloudwatch_log_group" "analyze" {
 resource "aws_api_gateway_resource" "translate" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "analyze"
+  path_part   = "translate"
 }
 
 resource "aws_api_gateway_method" "translate" {
