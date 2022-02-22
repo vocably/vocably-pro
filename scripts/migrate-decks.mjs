768880601128396712ae777905af1650b0aa6a68
Dima Snisarenko <snisarenkodima@gmail.com>
2022-02-22 11:11:50 +0100

    chore: add decks migrator script

---
+++ scripts/migrate-decks.mjs
@@ -0,0 +1,52 @@
+import 'zx/globals';
+import pkg from 'aws-sdk';
+import { mapKeys } from 'lodash-es';
+const { S3 } = pkg;
+
+const bucket = 'vocably-dev-cards';
+const s3Client = new S3();
+
+const objects = (
+  await s3Client
+    .listObjects({
+      Bucket: bucket,
+    })
+    .promise()
+).Contents;
+
+for (let object of objects) {
+  const deck = JSON.parse(
+    (
+      await s3Client
+        .getObject({
+          Bucket: bucket,
+          Key: object.Key,
+        })
+        .promise()
+    ).Body.toString()
+  );
+
+  console.log(deck);
+
+  for (let card of deck.cards) {
+    card.data = mapKeys(card.data, (value, key) => {
+      switch (key) {
+        case 'sideA':
+          return 'source';
+        case 'sideB':
+          return 'definition';
+        default:
+          return key;
+      }
+    });
+  }
+
+  await s3Client
+    .putObject({
+      Bucket: bucket,
+      Key: object.Key,
+      Body: JSON.stringify(deck),
+      ContentType: 'application/json',
+    })
+    .promise();
+}
