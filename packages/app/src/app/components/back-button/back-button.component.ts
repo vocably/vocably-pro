6b08337bc0b34eb23957c1f6cbda8c11acfd1f8b
Dima Snisarenko <snisarenkodima@gmail.com>
2022-02-24 11:49:05 +0100

    feat(app): beautiful back button

---
+++ packages/app/src/app/components/back-button/back-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  templateUrl: './back-button.component.html',
+})
+export class BackButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- packages/app/src/app/components/components.module.ts
+++ packages/app/src/app/components/components.module.ts
@@ -5,10 +5,20 @@ import { RouterModule } from '@angular/router';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { TooltipComponent } from './tooltip/tooltip.component';
 import { TooltipTriggerForDirective } from './tooltip/tooltip-trigger-for.directive';
+import { BackButtonComponent } from './back-button/back-button.component';
 
 @NgModule({
-  declarations: [TooltipComponent, TooltipTriggerForDirective],
+  declarations: [
+    TooltipComponent,
+    TooltipTriggerForDirective,
+    BackButtonComponent,
+  ],
   imports: [CommonModule, IonicModule, RouterModule, OverlayModule],
-  exports: [TooltipComponent, TooltipComponent, TooltipTriggerForDirective],
+  exports: [
+    TooltipComponent,
+    TooltipComponent,
+    TooltipTriggerForDirective,
+    BackButtonComponent,
+  ],
 })
 export class ComponentsModule {}
