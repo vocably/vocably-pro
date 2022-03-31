c6dd1168d639895bbf4b63236dd9c33242612a7c
Dima Snisarenko <snisarenkodima@gmail.com>
2022-03-31 09:46:01 +0200

    fix(app): consider cards created simultaniously when sorting by date

--- packages/app/src/app/decks/by-date.ts
+++ packages/app/src/app/decks/by-date.ts
@@ -1,4 +1,17 @@
 import { CardItem } from '@vocably/model';
 
-export const byDate = (a: CardItem, b: CardItem): number =>
-  b.created - a.created;
+export const byDate = (a: CardItem, b: CardItem): number => {
+  let k = b.created - a.created;
+
+  if (k !== 0) {
+    return k;
+  }
+
+  k = a.data.source.localeCompare(b.data.source);
+
+  if (k !== 0) {
+    return k;
+  }
+
+  return b.data.partOfSpeech.localeCompare(b.data.partOfSpeech);
+};
