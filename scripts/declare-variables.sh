409d65572205bc96f645bf6968ee1ae968cf1027
Dima Snisarenko <snisarenkodima@gmail.com>
2022-04-14 18:35:58 +0200

    ci: use lexicala_host and lexicala_key variables

--- scripts/declare-variables.sh
+++ scripts/declare-variables.sh
@@ -4,8 +4,8 @@ declare -a variables=(
   "AWS_ACCESS_KEY_ID"
   "AWS_SECRET_ACCESS_KEY"
   "GOOGLE_CREDENTIALS"
-  "TF_VAR_lexicala_username"
-  "TF_VAR_lexicala_password"
+  "TF_VAR_lexicala_host"
+  "TF_VAR_lexicala_key"
   "TF_VAR_google_oauth2_client_secret"
   "TF_VAR_test_user_password"
 )
