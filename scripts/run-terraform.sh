05e36b1842279593a129b3754d44d8f25dc8575f
Dima Snisarenko <snisarenkodima@gmail.com>
2022-05-03 16:16:56 +0200

    chore(tf): goup terraform environment files under env- prefix

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ./platform
-terraform init -backend-config=./${env_name}.remote
+terraform init -backend-config=./env-${env_name}.remote
 terraform workspace select ${env_name}
-terraform plan -var-file="${env_name}.tfvars"
-terraform apply -var-file="${env_name}.tfvars" -auto-approve
+terraform plan -var-file="env-${env_name}.tfvars"
+terraform apply -var-file="env-${env_name}.tfvars" -auto-approve
