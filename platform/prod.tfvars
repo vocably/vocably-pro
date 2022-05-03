0ac8a78113bc82e41bd4d3828298e83a1269fa34
Dima Snisarenko <snisarenkodima@gmail.com>
2022-05-03 15:17:38 +0200

    chore: set up paddle environment variables

--- platform/prod.tfvars
+++ platform/prod.tfvars
@@ -4,5 +4,67 @@ google_oauth2_client_id = "356478205648-le03fnat0mcc5ro8u9d7bphr5gsvgveb.apps.go
 extension_name          = "Vocably"
 extension_key           = ""
 extension_id            = "nomaniapkcfgfdgdadfojehechmghpgl"
-paddle_test_env         = "prod"
 piwik_id                = "25473e53-4fde-4fe1-a2ce-273ec6a4d0e3"
+paddle_vendor_id        = 142693
+paddle_sandbox          = false
+paddle_test_env         = "prod"
+paddle_subscription_products = [
+  {
+    duration = 1,
+    id       = 769580,
+    price    = 9.95,
+    currency = "EUR",
+    trial    = 7,
+  },
+  {
+    duration = 3,
+    id       = 769584,
+    price    = 19.95,
+    currency = "EUR",
+    trial    = 7,
+  },
+  {
+    duration = 12,
+    id       = 769582,
+    price    = 59.40,
+    currency = "EUR",
+    trial    = 7,
+  },
+  {
+    duration = 1,
+    id       = 769581,
+    price    = 9.95,
+    currency = "EUR",
+    trial    = 0,
+  },
+  {
+    duration = 3,
+    id       = 769585,
+    price    = 19.95,
+    currency = "EUR",
+    trial    = 0,
+  },
+  {
+    duration = 12,
+    id       = 769583,
+    price    = 59.40,
+    currency = "EUR",
+    trial    = 0,
+  }
+]
+paddle_public_key = <<PUBLIC_KEY
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA16+BMYl+UFJ6DJPcJFjo
+2RoTchirYQ5ZqSx3pJyCvTAlMmSa/vVSt1cJuqppDDZuTix/N9XuIVC1vjAU7n6Y
+fuvezpmHmHlq56YRp5B5ieoodiqIBU6M5iWluOGJKUnXWS+FtXInFvQXM2Dg86uX
+K1zmzz/ImacwKrFt0kEgFOA6ipVrafSGMdUPUlWcMMauWo5QP5Tgsp1EQPIOMKMR
+GyprCqNj4SJRG5boFIiuxEDISjQrvPT1rFIUUz0l2QYb9s3mAfntVUbqQGO8q4lA
+b+rlpVUUiOuJdtx+yMrfbLkPapvLmgjeMGpRo/qqLwl3+vRnKm+zhlXWviT0t9ge
+/iUutxZebC4E6XmowM0x/3vmfQQ/EDX+WFOcRO8bzLvrKdk3jujlKOAxlJU+LK2k
+3Eq/L38uR3CdRMgtGdmX/L+2M7R8PvZG/+tQUnPZNfsha3BujeY22/wPaa5hGA/a
+JubwiIuIZ69trbZlVSj/IMoGinA3Ak1znxSSPJi14YJ9TeD6GP8WIty+GD3e8vYT
+wFnx0JttV5c2pwI36rsDtMDnUpiX4/lf3PitDnQAq2f8CK5z8GvPGFhFSH5A/kFK
+wM4fdqZYLxjfb+NvGY1BMHZ5NQ1L9959jyrIf/yatOMMxddodezYqVDeg6ZOIWPd
+6xJNKFeNyPQZs542ASMMXB0CAwEAAQ==
+-----END PUBLIC KEY-----
+PUBLIC_KEY
--- platform/variables.tf
+++ platform/variables.tf
@@ -54,13 +54,15 @@ variable "test_user_password" {
 }
 
 variable "paddle_sandbox" {
-  type    = bool
-  default = true
+  type = bool
 }
 
 variable "paddle_vendor_id" {
-  type    = number
-  default = 5064
+  type = number
+}
+
+variable "paddle_test_env" {
+  type = string
 }
 
 variable "paddle_subscription_products" {
@@ -71,73 +73,8 @@ variable "paddle_subscription_products" {
     currency = string,
     trial    = number,
   }))
-  default = [
-    {
-      duration = 1,
-      id       = 26950,
-      price    = 9.95,
-      currency = "EUR",
-      trial    = 7,
-    },
-    {
-      duration = 3,
-      id       = 27076,
-      price    = 19.95,
-      currency = "EUR",
-      trial    = 7,
-    },
-    {
-      duration = 12,
-      id       = 26952,
-      price    = 59.40,
-      currency = "EUR",
-      trial    = 7,
-    },
-    {
-      duration = 1,
-      id       = 26951,
-      price    = 9.95,
-      currency = "EUR",
-      trial    = 0,
-    },
-    {
-      duration = 3,
-      id       = 27077,
-      price    = 19.95,
-      currency = "EUR",
-      trial    = 0,
-    },
-    {
-      duration = 12,
-      id       = 26953,
-      price    = 59.40,
-      currency = "EUR",
-      trial    = 0,
-    }
-  ]
 }
 
 variable "paddle_public_key" {
-  type    = string
-  default = <<PUBLIC_KEY
------BEGIN PUBLIC KEY-----
-MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAysQlrMGkV+sSpRpMDi0T
-V0salCD2SRwDjjq3gwNflu6jR/yJMOjRPqujNyZ/blsvntYrynsyJEAV28RaROZt
-8z7jaYCG5/1Vw4pqs79nbI+QB2PHJb8LLshH+IjNj5wrt25Zj47sKR+JxP0YG+W7
-OIcLOlkzwmqgJH7bMJDHGO2U3nEza5sHWSvtiSuc73G5ZzJ8jsBLLnwbP5KlddH4
-sXb8hYpTXnRQNGLd0jLvVMs/f0Ytj3uBM4Z1wIb9HstEgbC/tgd+1bupokW0ovNs
-EXoADrMQQK1ZQEZjZCpYNPfJKR/bZiumDSVzbr7RdPEfjkXueJMOlK9HTHOlHY+j
-Kl3YZSfng8HjWJySaTHhEGfC3voJJ1xIJEKbB0FdihLGes9DBrTTtWif9jkhNJut
-nvjWkwWlLO6LUtHvUrXH2tajOaY6vl4Zr9XcmPBw8rRp+lm7tMwLky8r4bqe1a/i
-g8yWgNZJqprZpy8phiPsHrdiPYWuNb65XxOuL7c9Vis9JJINJPIRx+1XMMBLPTVh
-P7Ni25J7enekPD4fjj9quMFlSwAyByvWwQ3ARkzzl+qm7aW6qwNl7VUGprvAPerc
-ss0prnN5T3x4uQeTPjdeQhtI5SMLkOxi0SDtz9M81q74dEcPvAJmHBO67OisKLms
-7IHnRcfOfFCzbPIgrakVnSECAwEAAQ==
------END PUBLIC KEY-----
-PUBLIC_KEY
-}
-
-variable "paddle_test_env" {
-  type    = string
-  default = "dev"
+  type = string
 }
