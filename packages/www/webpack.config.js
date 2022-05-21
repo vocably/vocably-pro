e3bcd5a5af081aeb4a81e88f86c241fe3829f00c
Dima Snisarenko <snisarenkodima@gmail.com>
2022-05-21 10:53:36 +0200

    feat(www): privacy policy and terms and conditions

--- packages/www/webpack.config.js
+++ packages/www/webpack.config.js
@@ -3,69 +3,81 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const WebpackWatchPlugin = require('webpack-watch-files-plugin').default;
 const { environment } = require('./environment');
+const glob = require('glob');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'resolve-url-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.handlebars$/,
-        use: [
-          {
-            loader: 'handlebars-loader',
-            options: {
-              helperDirs: `${__dirname}/handlebars-helpers`,
+const pagesDir = `./src/pages`;
+const handlebarsExtension = `handlebars`;
+const pagesPattern = `**/*${handlebarsExtension}`;
+
+module.exports = (env) => {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'resolve-url-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.handlebars$/,
+          use: [
+            {
+              loader: 'handlebars-loader',
+              options: {
+                helperDirs: [`${__dirname}/handlebars-helpers`],
+                partialDirs: [`${__dirname}/src/partials`],
+              },
             },
-          },
-        ],
-      },
-      { test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/, type: 'asset/resource' },
+          ],
+        },
+        { test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/, type: 'asset/resource' },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.png', '.svg'],
+    },
+    devtool: 'source-map',
+    output: {
+      chunkFilename: '[name].[contenthash].js',
+      filename: '[name].[contenthash].js',
+      assetModuleFilename: '[name].[contenthash][ext][query]',
+    },
+    plugins: [
+      ...glob.sync(pagesPattern, { cwd: pagesDir }).map(
+        (handlebarsPage) =>
+          new HtmlWebpackPlugin({
+            template: `${pagesDir}/${handlebarsPage}`,
+            filename: handlebarsPage.replace(handlebarsExtension, 'html'),
+            inject: true,
+            favicon: './src/favicon.ico',
+            environment,
+          })
+      ),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: 'src/assets', to: 'assets' }],
+      }),
+      new WebpackWatchPlugin({
+        files: [`${pagesDir}/${pagesPattern}`, './environment.js'],
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.png', '.svg'],
-  },
-  devtool: 'source-map',
-  output: {
-    chunkFilename: '[name].[contenthash].js',
-    filename: '[name].[contenthash].js',
-    assetModuleFilename: '[name].[contenthash][ext][query]',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/pages/index.handlebars',
-      inject: true,
-      favicon: './src/favicon.ico',
-      environment,
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-    new CopyPlugin({
-      patterns: [{ from: 'src/assets', to: 'assets' }],
-    }),
-    new WebpackWatchPlugin({
-      files: ['./environment.js'],
-    }),
-  ],
-  devServer: {
-    port: 8050,
-    hot: false,
-    liveReload: true,
-  },
+    devServer: {
+      port: 8050,
+      hot: false,
+      liveReload: true,
+    },
+  };
 };
