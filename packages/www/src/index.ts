5fe23a8e865fa78e51c65c594eabe86e1dd76c84
Dima Snisarenko <snisarenkodima@gmail.com>
2022-05-23 16:51:34 +0200

    feat(www): chrome extension disclaimer

--- packages/www/src/index.ts
+++ packages/www/src/index.ts
@@ -1,1 +1,16 @@
 import './styles.scss';
+import * as Bowser from 'bowser';
+
+const browser = Bowser.getParser(window.navigator.userAgent);
+
+const canInstallTheExtension = browser.satisfies({
+  desktop: {
+    chrome: '>1',
+  },
+});
+
+if (canInstallTheExtension !== true) {
+  document
+    .querySelectorAll('.browser-warning')
+    .forEach((element) => element.classList.remove('invisible'));
+}
