71c1682787cd207d9077cd191d7b62b37efeaac5
Dima Snisarenko <snisarenkodima@gmail.com>
2022-06-23 13:28:48 +0200

    chore(e2e): reset user under scripts dir

--- scripts/run-e2e-tests.mjs
+++ scripts/run-e2e-tests.mjs
@@ -1,9 +1,10 @@
 import 'zx/globals';
 import { config } from 'dotenv-flow';
+import { scriptsDir } from './helpers/dirs.mjs';
 
 config();
 
-await $`node ./reset-user.mjs`;
+await $`node ${scriptsDir}/reset-user.mjs`;
 
 await $`./endtest.sh ${process.env.ENDTEST_APP_ID} ${process.env.ENDTEST_APP_CODE} "https://app.endtest.io/api.php?action=runWeb&appId=${process.env.ENDTEST_APP_ID}&appCode=${process.env.ENDTEST_APP_CODE}&suite=${process.env.ENDTEST_LATEST_ENV_SUITE}&platform=mac&os=bigsur&browser=chrome&browserVersion=latest&resolution=1280x1024&geolocation=sanfrancisco&cases=all&notes=" 5`;
 
