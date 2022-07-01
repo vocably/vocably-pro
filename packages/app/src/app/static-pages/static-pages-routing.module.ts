ef47187014ae7bf1639bf3a16c420f3664074d86
Dima Snisarenko <snisarenkodima@gmail.com>
2022-07-01 10:34:26 +0200

    feat(app): get rid of tos and pp pages

--- packages/app/src/app/static-pages/static-pages-routing.module.ts
+++ packages/app/src/app/static-pages/static-pages-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TermsAndConditionsPageComponent } from './terms-and-conditions-page/terms-and-conditions-page.component';
-import { PrivacyPolicyPageComponent } from './privacy-policy-page/privacy-policy-page.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { UserInfoPageComponent } from './user-info-page/user-info-page.component';
 
 const routes: Routes = [
-  {
-    path: 'terms-and-conditions',
-    component: TermsAndConditionsPageComponent,
-  },
-  {
-    path: 'privacy-policy',
-    component: PrivacyPolicyPageComponent,
-  },
   {
     path: 'welcome',
     title: 'Welcome',
--- packages/app/src/app/static-pages/static-pages.module.ts
+++ packages/app/src/app/static-pages/static-pages.module.ts
@@ -3,20 +3,13 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ComponentsModule } from '../components/components.module';
 import { HeaderModule } from '../header/header.module';
-import { TermsAndConditionsPageComponent } from './terms-and-conditions-page/terms-and-conditions-page.component';
-import { PrivacyPolicyPageComponent } from './privacy-policy-page/privacy-policy-page.component';
 import { StaticPagesRoutingModule } from './static-pages-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AuthModule } from '../auth/auth.module';
 import { UserInfoPageComponent } from './user-info-page/user-info-page.component';
 
 @NgModule({
-  declarations: [
-    TermsAndConditionsPageComponent,
-    PrivacyPolicyPageComponent,
-    WelcomeComponent,
-    UserInfoPageComponent,
-  ],
+  declarations: [WelcomeComponent, UserInfoPageComponent],
   imports: [
     CommonModule,
     StaticPagesRoutingModule,
