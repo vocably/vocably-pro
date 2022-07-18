d4f81e6503043ba96ec9f7db2dc99367aa393843
Dima Snisarenko <snisarenkodima@gmail.com>
2022-07-18 12:03:25 +0200

    feat(app): disable language selector for the non-installed extension

--- packages/app/src/app/components/language-input/language-input.component.ts
+++ packages/app/src/app/components/language-input/language-input.component.ts
@@ -10,9 +10,21 @@ import { map, Observable, startWith } from 'rxjs';
 })
 export class LanguageInputComponent implements OnInit {
   @Input() value: Language = 'en';
+  private _disabled = false;
+  @Input() set disabled(disabled: boolean) {
+    this._disabled = disabled;
+    if (this._disabled) {
+      this.languageInput.disable();
+    } else {
+      this.languageInput.enable();
+    }
+  }
   @Output() onSelect = new EventEmitter<Language>();
 
-  languageInput = new FormControl<Language>(this.value);
+  languageInput = new FormControl<Language>({
+    value: 'en',
+    disabled: this._disabled,
+  });
 
   public languages$: Observable<Language[]> =
     this.languageInput.valueChanges.pipe(
