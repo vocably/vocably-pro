851918fea80139e0c966374a1dfa550555ab8780
Dima Snisarenko <dima.snisarenko@klm.com>
2023-09-25 11:22:11 +0300

    test: skip word dictionary test before key set

--- packages/backend/src/word-dictionary.spec.ts
+++ packages/backend/src/word-dictionary.spec.ts
@@ -2,6 +2,11 @@ import { wordDictionary } from "./word-dictionary";
 import { inspect } from "./utils/inspect";
 
 describe('wordDictionary', () => {
+  if (process.env.TEST_SKIP_SPEC === 'true') {
+    it('skip spec testing', () => {});
+    return;
+  }
+
   it('successfully fetches data from Word Dictionary', async () => {
     const result = await wordDictionary('stipulation');
     console.log(inspect(result));
