ad720638e524bb2dba0faea4511dc27191057113
Dima Snisarenko <snisarenkodima@gmail.com>
2024-03-08 10:39:00 +0900

    feat(moa): ask-for-review message simiar to extension

--- mobile-app/src/AskForReview/Form.tsx
+++ mobile-app/src/AskForReview/Form.tsx
@@ -1,18 +1,44 @@
+import { useNavigation } from '@react-navigation/native';
 import { RateInteractionPayload } from '@vocably/model';
 import React, { FC } from 'react';
-import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
+import {
+  ScrollView,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { Button, Text, useTheme } from 'react-native-paper';
 import { mobileStoreName } from '../mobilePlatform';
 
+const gradientHeight = 100;
+
 const styles = StyleSheet.create({
   container: {
+    maxHeight: 300,
+  },
+
+  scrollContainer: {
     flex: 0,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 24,
+    paddingBottom: gradientHeight - 30,
   },
   element: {
     marginTop: 12,
+  },
+  gradientPanel: {
+    position: 'absolute',
+    display: 'flex',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: gradientHeight,
+    pointerEvents: 'none',
   },
 });
 
@@ -26,37 +52,56 @@ export const AskForReviewForm: AskForReviewForm = ({
   onAction = () => {},
 }) => {
   const theme = useTheme();
+  const navigation = useNavigation();
   return (
-    <View style={[styles.container, style]}>
-      <View>
-        <Text style={{ fontWeight: 'bold' }}>
-          You can help other language learners.
-        </Text>
-      </View>
-      <View style={styles.element}>
-        <Text>Vocably is 100% free and open-source.</Text>
-      </View>
-      <View style={styles.element}>
-        <Text style={{ textAlign: 'center' }}>
-          Your positive review on {mobileStoreName} would help other users to
-          discover this project.
-        </Text>
-      </View>
-      <View style={[styles.element, { alignSelf: 'stretch' }]}>
-        <Button mode={'contained'} onPress={() => onAction('review')}>
-          Rate on {mobileStoreName}
-        </Button>
-      </View>
-      <View style={[styles.element, { alignSelf: 'stretch' }]}>
-        <Button mode={'outlined'} onPress={() => onAction('later')}>
-          Ask me later
-        </Button>
-      </View>
-      <View style={styles.element}>
-        <Button mode="text" onPress={() => onAction('never')}>
-          Please don't ask me about this.
-        </Button>
-      </View>
+    <View style={[styles.container]}>
+      <ScrollView contentContainerStyle={[styles.scrollContainer, style]}>
+        <View>
+          <Text style={{ fontWeight: 'bold' }}>You can help this project.</Text>
+        </View>
+        <View style={styles.element}>
+          <Text>Vocably is 100% free and open-source.</Text>
+        </View>
+        <View style={styles.element}>
+          <Text style={{ textAlign: 'center' }}>
+            Your positive review on {mobileStoreName} would help other users to
+            discover this project.
+          </Text>
+        </View>
+        <View style={styles.element}>
+          <Text style={{ textAlign: 'center' }}>
+            If you are missing or don't like something, you can always{' '}
+            <Text
+              style={{ color: theme.colors.primary }}
+              onPress={() => navigation.navigate('Feedback')}
+            >
+              let me know
+            </Text>
+            . I take every feedback seriously.
+          </Text>
+        </View>
+        <View style={[styles.element, { alignSelf: 'stretch' }]}>
+          <Button mode={'contained'} onPress={() => onAction('review')}>
+            Rate on {mobileStoreName}
+          </Button>
+        </View>
+        <View style={[styles.element, { alignSelf: 'stretch' }]}>
+          <Button mode={'outlined'} onPress={() => onAction('later')}>
+            Ask me later
+          </Button>
+        </View>
+        <View style={styles.element}>
+          <Button mode="text" onPress={() => onAction('never')}>
+            Please don't ask me about this.
+          </Button>
+        </View>
+      </ScrollView>
+      <LinearGradient
+        locations={[0.1, 1]}
+        // @ts-ignore
+        colors={[theme.colors.transparentSurface, theme.colors.surface]}
+        style={[styles.gradientPanel]}
+      ></LinearGradient>
     </View>
   );
 };
