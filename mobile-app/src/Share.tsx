88ac4143b36f27a02828245d27177dfcc95b6d0a
bekatru <teamusicband@gmail.com>
2024-03-14 14:02:35 +0600

    Update Share component

--- mobile-app/src/Share.tsx
+++ mobile-app/src/Share.tsx
@@ -1,9 +1,44 @@
-import { Text, View } from "react-native"
+import { useCallback, useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import ShareMenu, { ShareData } from 'react-native-share-menu';
 
 export const Share = () => {
-    return (
-        <View>
-            <Text>Hello</Text>
-        </View>
-    )
-}
+  const [sharedText, setSharedText] = useState<string>(
+    ""
+  );
+
+  const handleShare = useCallback((data?: ShareData) => {
+    if (data) {
+      if (Array.isArray(data.data)) {
+        setSharedText(data.data.join(', '))
+      } else {
+        setSharedText(data.data);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    ShareMenu.getInitialShare(handleShare);
+  }, []);
+
+  useEffect(() => {
+    const listener = ShareMenu.addNewShareListener(handleShare);
+
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        backgroundColor: 'gray',
+        padding: 24,
+        borderRadius: 16,
+      }}
+    >
+      <Text style={{ fontSize: 32 }}>{sharedText}</Text>
+    </View>
+  );
+};
