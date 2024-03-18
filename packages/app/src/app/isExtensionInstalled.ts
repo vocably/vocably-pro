d106ec6c5dca1d68b5cb37ef14c29e0f372bfbf8
Dima Snisarenko <snisarenkodima@gmail.com>
2024-03-19 08:25:39 +0900

    fix(app): properly verify intalled extension in safari

--- packages/app/src/app/isExtensionInstalled.ts
+++ packages/app/src/app/isExtensionInstalled.ts
@@ -1,8 +1,13 @@
 import { pingExternal } from '@vocably/extension-messages';
-import { defer, map, retry } from 'rxjs';
+import { defer, map, retry, tap } from 'rxjs';
 import { extensionId } from '../extension-id';
 
 export const isExtensionInstalled = defer(() => pingExternal(extensionId)).pipe(
+  tap((result) => {
+    if (result !== 'pong') {
+      throw new Error('Extension is not installed');
+    }
+  }),
   map(() => true),
   retry({
     delay: 2000,
