b6a52d2320d7d6dfdbb38140607c111d9122229f
bekatru <teamusicband@gmail.com>
2024-03-26 12:30:08 +0600

    Add authentication guard

--- mobile-app/src/Share.tsx
+++ mobile-app/src/Share.tsx
@@ -1,16 +1,39 @@
-import { useCallback, useEffect, useState } from 'react';
-import { View, Text, Button, BackHandler } from 'react-native';
+import {
+  FC,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { BackHandler, Button, Linking, Text, View } from 'react-native';
 import ShareMenu, { ShareData } from 'react-native-share-menu';
+import { AuthContainer } from './auth/AuthContainer';
+import { AuthContext } from './auth/AuthContext';
+import { Loader } from './loaders/Loader';
+
+const MAIN_APP_URL_SCHEME = 'vocably-pro://';
+
+const openAnotherApp = () => {
+
+  Linking.canOpenURL(MAIN_APP_URL_SCHEME)
+    .then((supported) => {
+      if (supported) {
+        Linking.openURL(MAIN_APP_URL_SCHEME);
+      } else {
+        console.log(`Can't handle url: ${MAIN_APP_URL_SCHEME}`);
+      }
+    })
+    .catch((err) => console.error('An error occurred', err));
+};
 
 export const useShare = () => {
-  const [sharedText, setSharedText] = useState<string>(
-    ""
-  );
+  const [sharedText, setSharedText] = useState<string>('');
 
   const handleShare = useCallback((data?: ShareData) => {
     if (data) {
       if (Array.isArray(data.data)) {
-        setSharedText(data.data.join(', '))
+        setSharedText(data.data.join(', '));
       } else {
         setSharedText(data.data);
       }
@@ -29,26 +52,60 @@ export const useShare = () => {
     };
   }, []);
   return sharedText;
-}
+};
+
+export const Login: FC<{
+  children?: ReactNode;
+}> = ({ children }) => {
+  const authStatus = useContext(AuthContext);
+
+  if (authStatus.status === 'undefined') {
+    return <Loader>Authenticating...</Loader>;
+  }
+
+  if (authStatus.status === 'logged-in') {
+    return <>{children}</>;
+  }
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View
+        style={{
+          alignItems: 'center',
+          backgroundColor: 'gray',
+          padding: 24,
+          borderRadius: 16,
+        }}
+      >
+        <Text style={{ fontSize: 24 }}>You are not logged in</Text>
+        <Button title="Login" onPress={openAnotherApp} />
+      </View>
+    </View>
+  );
+};
 
 const Share = () => {
-
   const sharedText = useShare();
-  
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <View
-      style={{
-        alignItems: 'center',
-        backgroundColor: 'gray',
-        padding: 24,
-        borderRadius: 16,
-      }}
-    >
-      <Text style={{ fontSize: 32 }}>{sharedText}</Text>
-      <Button title='Done' onPress={() => BackHandler.exitApp()}/>
-    </View>
-    </View>
+    <AuthContainer>
+      <Login>
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <View
+            style={{
+              alignItems: 'center',
+              backgroundColor: 'gray',
+              padding: 24,
+              borderRadius: 16,
+            }}
+          >
+            <Text style={{ fontSize: 32 }}>{sharedText}</Text>
+            <Button title="Done" onPress={() => BackHandler.exitApp()} />
+          </View>
+        </View>
+      </Login>
+    </AuthContainer>
   );
 };
 
