45b120f2e74aa08c18c139f65bbeb470d037e3e2
Dima Snisarenko <snisarenkodima@gmail.com>
2024-03-31 16:43:34 +0200

    set source language

--- packages/app/src/app/welcome/pages/second-page/second-page.component.ts
+++ packages/app/src/app/welcome/pages/second-page/second-page.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { filter, Subject, switchMap, takeUntil } from 'rxjs';
+import { setSourceLanguage } from '@vocably/extension-messages';
+import { isGoogleLanguage } from '@vocably/model';
+import { filter, map, Subject, switchMap, takeUntil, tap } from 'rxjs';
+import { extensionId } from '../../../../extension';
 
 @Component({
   selector: 'app-second-page',
@@ -24,15 +27,18 @@ export class SecondPageComponent implements OnInit, OnDestroy {
       .pipe(
         takeUntil(this.destroy$),
         filter((params: any) => !!params['language']),
-        switchMap((params) => {
-          this.isLoading = true;
-          return this.httpClient.get(
-            `/assets/language-text-examples/${params['language']}.html`,
+        map((params) => params['language']),
+        filter(isGoogleLanguage),
+        tap(() => (this.isLoading = true)),
+        tap((language) => setSourceLanguage(extensionId, language)),
+        switchMap((language) =>
+          this.httpClient.get(
+            `/assets/language-text-examples/${language}.html`,
             {
               responseType: 'text',
             }
-          );
-        }),
+          )
+        ),
         takeUntil(this.destroy$)
       )
       .subscribe((response) => {
