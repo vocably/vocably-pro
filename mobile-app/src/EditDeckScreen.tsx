b908d888c65e38342738d2d6fd9f89fd1406c908
bekatru <teamusicband@gmail.com>
2024-04-17 15:23:46 +0600

    Update EditDeckScreen and EditDeckMenu

--- mobile-app/src/EditDeckScreen.tsx
+++ mobile-app/src/EditDeckScreen.tsx
@@ -1,109 +1,28 @@
 import { NavigationProp } from '@react-navigation/native';
-import { byDate, CardItem } from '@vocably/model';
-import React, { FC, useCallback, useMemo, useState } from 'react';
+import React, { FC } from 'react';
 import {
-  Alert,
-  ListRenderItemInfo,
-  Pressable,
   StyleSheet,
   View,
 } from 'react-native';
-import { ActivityIndicator, Text, useTheme } from 'react-native-paper';
-import { SwipeListView } from 'react-native-swipe-list-view';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { CardListItem, keyExtractor, Separator } from './CardListItem';
-import { EmptyCardsList } from './EmptyCardsList';
-import { userSelectedDeck } from './languageDeck/userSelectedDeck';
+import { Menu, Text } from 'react-native-paper';
+import { EditDeckMenu } from './EditDeckMenu';
+import { mainPadding } from './styles';
 
-const SWIPE_MENU_BUTTON_SIZE = 50;
 
 type EditDeckScreen = FC<{
   navigation: NavigationProp<any>;
 }>;
 
 export const EditDeckScreen: EditDeckScreen = ({ navigation }) => {
-  const theme = useTheme();
-  const { deck, remove } = userSelectedDeck();
-  const cards = useMemo(() => deck.cards.sort(byDate), [deck]);
-
-  const [toBeDeletedId, setToBeDeletedId] = useState<string | null>(null);
-
-  const deleteCard = useCallback(
-    async (id: string) => {
-      setToBeDeletedId(id);
-      const deleteResult = await remove(id);
-      if (deleteResult.success === false) {
-        Alert.alert(
-          'Error: Card deletion failed',
-          `Oops! Something went wrong while attempting to delete the card. Please try again later.`
-        );
-      }
-      setToBeDeletedId(null);
-    },
-    [remove]
-  );
-
-  const renderCard = (data: ListRenderItemInfo<CardItem>) => (
-    <View style={{ backgroundColor: theme.colors.background }}>
-      <Pressable
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed
-              ? theme.colors.inversePrimary
-              : theme.colors.background,
-          },
-        ]}
-        onPress={() => navigation.navigate('EditCard', { card: data.item })}
-      >
-        <CardListItem card={data.item.data} />
-      </Pressable>
-    </View>
-  );
-
-  const renderSwipeMenu = (data: ListRenderItemInfo<CardItem>) => (
-    <View style={styles.swipeMenu}>
-      <Pressable
-        onPress={() => deleteCard(data.item.id)}
-        disabled={toBeDeletedId === data.item.id}
-        style={[styles.swipeButton, { backgroundColor: theme.colors.error }]}
-      >
-        {toBeDeletedId === data.item.id ? (
-          <ActivityIndicator size={32} color={theme.colors.onSecondary} />
-        ) : (
-          <Icon
-            name="delete-outline"
-            size={32}
-            color={theme.colors.onSecondary}
-          />
-        )}
-      </Pressable>
-    </View>
-  );
 
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          backgroundColor: theme.colors.background,
-        },
-      ]}
-    >
-      {!cards.length ? (
-        <EmptyCardsList>
-          <Text>Card list is empty</Text>
-        </EmptyCardsList>
-      ) : (
-        <SwipeListView<CardItem>
-          style={styles.swipeList}
-          data={cards}
-          ItemSeparatorComponent={Separator}
-          keyExtractor={keyExtractor}
-          renderItem={renderCard}
-          renderHiddenItem={renderSwipeMenu}
-          rightOpenValue={-SWIPE_MENU_BUTTON_SIZE}
-        />
-      )}
+    <View style={styles.container}>
+      <EditDeckMenu/>
+
+      <View style={styles.bottomInfo}>
+        <Text>More deck settings are comming soon.</Text>
+        <Text>Stay tuned!</Text>
+      </View>
     </View>
   );
 };
@@ -112,22 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     width: '100%',
+    paddingVertical: mainPadding,
   },
-  swipeList: {
-    width: '100%',
-  },
-  swipeButton: {
-    width: SWIPE_MENU_BUTTON_SIZE,
-    height: '100%',
-    justifyContent: 'center',
+  bottomInfo: {
     alignItems: 'center',
-  },
-  swipeMenu: {
-    height: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
+  }
 });
