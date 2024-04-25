e57d88ff63857bb69073511249cc0170e35b4399
Dima Snisarenko <snisarenkodima@gmail.com>
2024-04-25 17:32:12 +0200

    feat(onboarding): prettier onboarding

--- packages/app/src/app/components/expansion/expansion.component.ts
+++ packages/app/src/app/components/expansion/expansion.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ExpansionComponent implements OnInit {
   @Input() label: string = '';
+  @Input() panelStyle: string = '';
 
   isExpanded = false;
 
