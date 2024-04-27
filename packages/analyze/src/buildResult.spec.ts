7b124a0ab702119d30176043ee03273bcbbb4563
Dima Snisarenko <snisarenkodima@gmail.com>
2024-04-27 06:10:23 +0200

    fix(analyze): temporarily disable examples

--- packages/analyze/src/buildResult.spec.ts
+++ packages/analyze/src/buildResult.spec.ts
@@ -470,21 +470,22 @@ describe('integration check for translate lambda', () => {
     expect(result.value.items[0].translation).toContain('، ');
   });
 
-  it('provides lexicala examples', async () => {
-    const result = await buildResult({
-      sourceLanguage: 'en',
-      targetLanguage: 'ru',
-      source: 'sister',
-    });
-
-    console.log(inspect(result));
-
-    expect(result.success).toBeTruthy();
-    if (result.success === false) {
-      return;
-    }
-
-    expect(result.value.items[0].examples).toBeDefined();
-    expect(result.value.items[0].examples.length).toBeGreaterThan(0);
-  });
+  // ToDo: uncomment whe Chrome extension is released
+  // it('provides lexicala examples', async () => {
+  //   const result = await buildResult({
+  //     sourceLanguage: 'en',
+  //     targetLanguage: 'ru',
+  //     source: 'sister',
+  //   });
+  //
+  //   console.log(inspect(result));
+  //
+  //   expect(result.success).toBeTruthy();
+  //   if (result.success === false) {
+  //     return;
+  //   }
+  //
+  //   expect(result.value.items[0].examples).toBeDefined();
+  //   expect(result.value.items[0].examples.length).toBeGreaterThan(0);
+  // });
 });
--- packages/analyze/src/lexicala/lexicalaSearchResultToAnalysisItem.ts
+++ packages/analyze/src/lexicala/lexicalaSearchResultToAnalysisItem.ts
@@ -2,7 +2,6 @@ import { AnalysisItem, Translation } from '@vocably/model';
 import { addArticle } from '../addArticle';
 import { extractTranslation } from '../extractTranslation';
 import { getDefinitions } from './getDefinitions';
-import { getExamples } from './getExamples';
 import { LexicalaSearchResultItemWithNormalHeadword } from './normalizeHeadword';
 
 export const lexicalaSearchResultToAnalysisItem =
@@ -16,7 +15,9 @@ export const lexicalaSearchResultToAnalysisItem =
     );
     return {
       source: addArticle(item.language, item.headword),
-      examples: getExamples(item.senses),
+      // ToDo: uncomment the line below as soon as Chrome extension is releaed
+      // examples: getExamples(item.senses),
+      examples: [],
       definitions: getDefinitions(item.senses),
       partOfSpeech: item.headword.pos,
       translation: translationResult.success ? translationResult.value : '',
