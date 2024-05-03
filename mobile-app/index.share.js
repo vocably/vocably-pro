8669fc6c50dff998ce039141cecc10248de2cbf7
bekatru <teamusicband@gmail.com>
2024-05-03 15:11:44 +0600

    link rn-random-values

--- mobile-app/index.share.js
+++ mobile-app/index.share.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import ShareIntentApp from './src/ShareIntent/ShareIntentAppIos';
+import 'react-native-get-random-values';
 
 AppRegistry.registerComponent('ShareMenuModuleComponent', () => ShareIntentApp);
