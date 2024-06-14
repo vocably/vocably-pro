178b14ed428bf53535ecc4a528fe2fb99cca53a7
Dima Snisarenko <snisarenkodima@gmail.com>
2024-06-14 14:16:08 +0200

    feat(app): let users know about the email communication upfront

--- packages/app/src/app/pages/feedback-page/feedback-page.component.ts
+++ packages/app/src/app/pages/feedback-page/feedback-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { sendUserFeedback } from '@vocably/api';
-import { UserData } from '@vocably/model';
 import { AuthService } from '../../auth/auth.service';
 
 @Component({
@@ -12,6 +11,7 @@ export class FeedbackPageComponent implements OnInit {
   feedback = '';
   isSubmitting = false;
   success = false;
+  lastFeedback = '';
   error = false;
 
   userData$ = this.authService.fetchUserData$;
@@ -40,17 +40,10 @@ export class FeedbackPageComponent implements OnInit {
 
     if (res.success) {
       this.success = true;
+      this.lastFeedback = this.feedback;
       this.feedback = '';
     } else {
       this.error = true;
     }
-  }
-
-  public getEmailAddress(userData: UserData) {
-    if (userData.email.includes('privaterelay')) {
-      return '.';
-    }
-
-    return `: ${userData.email}.`;
   }
 }
