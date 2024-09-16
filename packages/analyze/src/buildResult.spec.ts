15937885f54a6536bae22692e3d94344751c014c
Dima Snisarenko <snisarenkodima@gmail.com>
2024-09-16 17:58:00 +0200

    chore(test): verify context translation

--- packages/analyze/src/buildResult.spec.ts
+++ packages/analyze/src/buildResult.spec.ts
@@ -577,4 +577,18 @@ describe('integration check for translate lambda', () => {
     expect(result.value.items[0].ipa).toEqual('nǐhǎo');
     expect(result.value.items[0].translation).toEqual('Привет');
   });
+
+  it('provides context translation', async () => {
+    const result = await buildResult({
+      source: 'bank',
+      sourceLanguage: 'en',
+      targetLanguage: 'ru',
+      context:
+        "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'",
+    });
+    if (result.success === false) {
+      throw 'Unexpected result';
+    }
+    expect(result.value.translation.target).toEqual('берег');
+  });
 });
