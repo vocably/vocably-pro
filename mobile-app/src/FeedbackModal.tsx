625f891aa25ecec8510aa754497f012572e73bb2
Dima Snisarenko <snisarenkodima@gmail.com>
2024-10-30 13:17:43 +0100

    feat(moa): feedback form reworked

--- mobile-app/src/FeedbackModal.tsx
+++ mobile-app/src/FeedbackModal.tsx
@@ -1,10 +1,11 @@
 import { NavigationProp } from '@react-navigation/native';
 import { sendUserFeedback } from '@vocably/api';
-import { FC, useCallback, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { Alert, Platform, ScrollView, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useUserEmail } from './auth/useUserEmail';
+import { useTranslationPreset } from './TranslationPreset/useTranslationPreset';
 import { VocablyTextInput } from './VocablyTextInput';
 
 type Props = {
@@ -16,6 +17,8 @@ export const FeedbackModal: FC<Props> = ({ navigation }) => {
   const [isSending, setIsSending] = useState(false);
   const userEmail = useUserEmail();
 
+  const [translationPreset] = useTranslationPreset();
+
   const sendFeedback = useCallback(async () => {
     setIsSending(true);
     const res = await sendUserFeedback({
@@ -23,12 +26,20 @@ export const FeedbackModal: FC<Props> = ({ navigation }) => {
       metadata: {
         platform: 'mobile',
         os: Platform.OS,
+        translationPreset,
       },
     });
 
     if (res.success === true) {
-      Alert.alert('Thank you for your feedback!');
-      setText('');
+      Alert.alert('Success', 'Thank you for your feedback.', [
+        {
+          text: 'Go back',
+          onPress: () => {
+            setText('');
+            navigation.goBack();
+          },
+        },
+      ]);
     } else {
       Alert.alert('Something went wrong. Please try again later.');
     }
@@ -36,68 +47,66 @@ export const FeedbackModal: FC<Props> = ({ navigation }) => {
     setIsSending(false);
   }, [sendUserFeedback, text, setIsSending, Alert, navigation.goBack]);
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          disabled={text.trim().length === 0 || isSending}
+          onPress={sendFeedback}
+          loading={isSending}
+          style={{ marginRight: 8 }}
+        >
+          Send
+        </Button>
+      ),
+    });
+  }, [text, sendFeedback, isSending]);
+
   const insets = useSafeAreaInsets();
 
   return (
-    <ScrollView automaticallyAdjustKeyboardInsets={true}>
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'stretch',
-          justifyContent: 'flex-start',
-          paddingVertical: 32,
-          paddingLeft: insets.left + 32,
-          paddingRight: insets.right + 32,
-          gap: 16,
-        }}
-      >
-        <Text>
-          Are you missing any crucial features? Do you have questions, or would
-          you like to share your opinion about Vocably? I would love to hear
-          from you!
-        </Text>
-        <Text>
-          I respond to every user. Usually, it takes me a couple of days to
-          reply.
-        </Text>
-        {userEmail && (
-          <Text>
-            I will respond to you at your email address{' '}
-            <Text style={{ fontWeight: 'bold' }}>{userEmail}</Text>.
-            {userEmail.includes('privaterelay') && (
-              <Text>
-                {' '}
-                This seems like a secret Apple email you shared with me during
-                the registration, but no worries; it should work just fine.
-              </Text>
-            )}
-          </Text>
-        )}
-        <VocablyTextInput
-          multiline
-          placeholder={'Please type your message here...'}
-          inputStyle={{ height: 128 }}
-          onChangeText={setText}
-          value={text}
-        />
+    <>
+      <ScrollView automaticallyAdjustKeyboardInsets={true}>
         <View
           style={{
-            flexDirection: 'row',
+            flex: 1,
+            alignItems: 'stretch',
+            justifyContent: 'flex-start',
+            paddingVertical: 32,
+            paddingLeft: insets.left + 32,
+            paddingRight: insets.right + 32,
             gap: 16,
-            justifyContent: 'flex-end',
           }}
         >
-          <Button onPress={() => navigation.goBack()}>Close</Button>
-          <Button
-            mode={'contained'}
-            disabled={text.trim() === '' || isSending}
-            onPress={sendFeedback}
-            loading={isSending}
-          >
-            Send
-          </Button>
+          <Text>
+            Missing any crucial features? Have questions or want to share your
+            thoughts on Vocably? I'd love to hear from you!
+          </Text>
+          <Text>
+            I personally respond to every user, usually within a couple of days.
+          </Text>
+          {userEmail && (
+            <Text>
+              I'll reply to you at your email address{' '}
+              <Text style={{ fontWeight: 'bold' }}>{userEmail}</Text>.
+              {userEmail.includes('privaterelay') && (
+                <Text>
+                  {' '}
+                  It looks like you shared a private Apple email with me during
+                  registration, but no worries — it should work just fine.
+                </Text>
+              )}
+            </Text>
+          )}
+          <VocablyTextInput
+            multiline
+            placeholder={'Please type your message here...'}
+            inputStyle={{ height: 128 }}
+            onChangeText={setText}
+            value={text}
+          />
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
--- mobile-app/src/RootModalStack.tsx
+++ mobile-app/src/RootModalStack.tsx
@@ -1,4 +1,6 @@
+import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Button, useTheme } from 'react-native-paper';
 import { DrawerNavigator } from './DrawerNavigator';
 import { FeedbackModal } from './FeedbackModal';
 import { LanguageSelectorModal } from './LanguageSelectorModal';
@@ -7,6 +9,7 @@ import { StudyScreen } from './study/StudyScreen';
 const Stack = createStackNavigator();
 
 export const RootModalStack = () => {
+  const theme = useTheme();
   return (
     <Stack.Navigator>
       <Stack.Group screenOptions={{ headerShown: false }}>
@@ -23,7 +26,27 @@ export const RootModalStack = () => {
           name="LanguageSelector"
           component={LanguageSelectorModal}
         />
-        <Stack.Screen name={'Feedback'} component={FeedbackModal} />
+        <Stack.Screen
+          name={'Feedback'}
+          component={FeedbackModal}
+          options={{
+            headerShown: true,
+            title: 'Feedback',
+            headerTintColor: theme.colors.onBackground,
+            headerRight: () => <Button disabled={true}>Send</Button>,
+            headerLeft: () => {
+              const navigation = useNavigation();
+              return (
+                <Button
+                  style={{ marginLeft: 8 }}
+                  onPress={() => navigation.goBack()}
+                >
+                  Close
+                </Button>
+              );
+            },
+          }}
+        />
       </Stack.Group>
     </Stack.Navigator>
   );
