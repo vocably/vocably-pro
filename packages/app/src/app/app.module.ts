6a8de382c9ee3eea7873ff6725ecfd9bc1af72fb
Dima Snisarenko <snisarenkodima@gmail.com>
2024-12-06 16:33:06 +0100

    display selected tags

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
@@ -48,6 +49,7 @@ import { TagsModule } from './tags/tags.module';
     MatDialogModule,
     MatButtonModule,
     MatDividerModule,
+    MatChipsModule,
     TagsModule,
   ],
   providers: [
