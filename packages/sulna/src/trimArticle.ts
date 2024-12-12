86321bd198e0266f372df9c0adae9364883ae352
Dima Snisarenko <snisarenkodima@gmail.com>
2024-12-12 08:27:32 +0100

    feat(backend): trim articles for en, es, fr, it, and pt

--- packages/sulna/src/trimArticle.ts
+++ packages/sulna/src/trimArticle.ts
@@ -4,8 +4,13 @@ type TrimArticleResult = {
 };
 
 const trimRegexes: Partial<Record<string, RegExp>> = {
+  en: /^(a)\s/i,
   nl: /^(de|het|de.het|het.de)\s/i,
-  de: /^(der|die|das)\s/i,
+  de: /^(der|die|das|ein|eine)\s/i,
+  es: /^(el|la|los|las)\s/i,
+  fr: /^(le|la|les|un|une|des|du|de)\s/i,
+  it: /^(il|lo|la|i|gli|le|un|uno|una)\s/i,
+  pt: /^(o|a|os|as|um|uma|uns|umas)\s/i,
 };
 
 export const trimArticle = (
