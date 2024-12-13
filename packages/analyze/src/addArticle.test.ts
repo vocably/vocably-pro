7444f170bfc62e529f4da2913ae9b0e58043ede3
Dima Snisarenko <snisarenkodima@gmail.com>
2024-12-13 11:01:10 +0100

    feat: definite articles in spanish and italian

--- packages/analyze/src/addArticle.test.ts
+++ packages/analyze/src/addArticle.test.ts
@@ -73,5 +73,85 @@ describe('addArticle', () => {
         gender: 'neuter',
       })
     ).toEqual('et hus');
+
+    expect(
+      addArticle('es', {
+        text: 'aqua',
+        pos: 'noun',
+        gender: 'feminine',
+      })
+    ).toEqual('el aqua');
+
+    expect(
+      addArticle('es', {
+        text: 'chica',
+        pos: 'noun',
+        gender: 'feminine',
+      })
+    ).toEqual('la chica');
+
+    expect(
+      addArticle('es', {
+        text: 'estudiante',
+        pos: 'noun',
+        gender: 'masculine-feminine',
+      })
+    ).toEqual('el/la estudiante');
+
+    expect(
+      addArticle('es', {
+        text: 'chico',
+        pos: 'noun',
+        gender: 'masculine',
+      })
+    ).toEqual('el chico');
+
+    expect(
+      addArticle('it', {
+        pos: 'noun',
+        gender: 'masculine',
+        text: 'libro',
+      })
+    ).toEqual('il libro');
+
+    expect(
+      addArticle('it', {
+        pos: 'noun',
+        gender: 'masculine',
+        text: 'studente',
+      })
+    ).toEqual('lo studente');
+
+    expect(
+      addArticle('it', {
+        pos: 'noun',
+        gender: 'masculine',
+        text: 'zaino',
+      })
+    ).toEqual('lo zaino');
+
+    expect(
+      addArticle('it', {
+        pos: 'noun',
+        gender: 'masculine',
+        text: 'amico',
+      })
+    ).toEqual("l'amico");
+
+    expect(
+      addArticle('it', {
+        pos: 'noun',
+        gender: 'feminine',
+        text: 'casa',
+      })
+    ).toEqual('la casa');
+
+    expect(
+      addArticle('it', {
+        pos: 'noun',
+        gender: 'feminine',
+        text: 'isola',
+      })
+    ).toEqual("l'isola");
   });
 });
--- packages/analyze/src/addArticle.ts
+++ packages/analyze/src/addArticle.ts
@@ -1,12 +1,18 @@
+import { isFunction } from 'lodash-es';
 import { Headword } from './lexicala';
 import { LexicalaLanguage } from './lexicala/lexicalaLanguageMapper';
+import { vowels } from './vowels';
 
 type ArticleRules = {
   [gender: string]: string;
   fallback: string;
 };
 
-const languageArticles: Partial<Record<LexicalaLanguage, ArticleRules>> = {
+type ArticleFunction = (options: { gender: string; word: string }) => string;
+
+const languageArticles: Partial<
+  Record<LexicalaLanguage, ArticleRules | ArticleFunction>
+> = {
   nl: {
     neuter: 'het ',
     'masculine-neuter': 'de/het ',
@@ -25,6 +31,57 @@ const languageArticles: Partial<Record<LexicalaLanguage, ArticleRules>> = {
     neuter: 'et ',
     fallback: '',
   },
+  es: ({ gender, word }) => {
+    const firstLetter = (word.at(0) ?? '').toLowerCase();
+    const secondLetter = (word.at(1) ?? '').toLowerCase();
+    if (firstLetter === 'a' || (firstLetter === 'h' && secondLetter === 'a')) {
+      return 'el ';
+    }
+
+    if (gender === 'masculine') {
+      return 'el ';
+    }
+
+    if (gender === 'feminine') {
+      return 'la ';
+    }
+
+    return 'el/la ';
+  },
+  it: ({ gender, word }) => {
+    const firstLetter = (word.at(0) ?? '').toLowerCase();
+    const secondLetter = (word.at(1) ?? '').toLowerCase();
+
+    if (gender === 'masculine') {
+      if (vowels.includes(firstLetter)) {
+        return "l'";
+      }
+
+      if (firstLetter === 's' && !vowels.includes(secondLetter)) {
+        return 'lo ';
+      }
+
+      if (['ps', 'gn'].includes(firstLetter + secondLetter)) {
+        return 'lo ';
+      }
+
+      if (firstLetter === 'x' || firstLetter === 'z') {
+        return 'lo ';
+      }
+
+      return 'il ';
+    }
+
+    if (gender === 'feminine') {
+      if (vowels.includes(firstLetter)) {
+        return "l'";
+      }
+
+      return 'la ';
+    }
+
+    return '';
+  },
 };
 
 export const addArticle = (
@@ -41,6 +98,15 @@ export const addArticle = (
 
   if (rules === undefined) {
     return text;
+  }
+
+  if (isFunction(rules)) {
+    return (
+      rules({
+        gender: headword.gender,
+        word: text,
+      }) + text
+    );
   }
 
   const article = rules[headword.gender] ?? rules.fallback;
