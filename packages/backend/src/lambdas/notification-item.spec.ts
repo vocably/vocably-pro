42c4b7a3e2e887a9ea0514c83ba67b386cde21d7
Dima Snisarenko <snisarenkodima@gmail.com>
2025-02-27 13:53:52 +0100

    chore(backend): move notificaion tests under the single file

--- packages/backend/src/lambdas/notification-item.spec.ts
+++ packages/backend/src/lambdas/notification-item.spec.ts
@@ -2,6 +2,7 @@ import { inspect } from '@vocably/node-sulna';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { deleteNotificationTimeLambdaFunction } from './delete-notification-time';
 import { getNotificationTimeLambdaFunction } from './get-notification-time';
+import { recalibrateNotificationsLambdaFunction } from './recalibrate-notifications';
 import { setNotificationTimeLambdaFunction } from './set-notification-time';
 
 // @ts-ignore
@@ -74,6 +75,15 @@ describe('integration check for getNotificationTime lambda', () => {
     expect(body.exists).toEqual(true);
     expect(body.language).toEqual('en');
     expect(body.time).toEqual('23:00');
+  });
+
+  it('recalibrate notifications lambda', async () => {
+    mockEvent.body = JSON.stringify({
+      IANATimezone: 'Europe/Amsterdam',
+    });
+    const result = await recalibrateNotificationsLambdaFunction(mockEvent);
+    console.log(inspect({ result }));
+    expect(result.statusCode).toEqual(200);
   });
 
   it('delete notification time', async () => {
