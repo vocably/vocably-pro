daee2a9eaf8f3ea840aa73a4b2bed7c591afef70
Dima Snisarenko <snisarenkodima@gmail.com>
2025-03-07 16:35:53 +0100

    chore: get user endpoints dashboard method

---
+++ dashboard/get-user-endpoints.mjs
@@ -0,0 +1,26 @@
+import { exec } from 'child_process';
+import { config } from 'dotenv-flow';
+import { inspect, promisify } from 'node:util';
+import 'zx/globals';
+
+config();
+
+const execute = promisify(exec);
+
+const userEmail = process.argv[2];
+
+console.log(process.env.USER_POOL_ID);
+
+const listUsers = `aws cognito-idp list-users --user-pool-id ${process.env.USER_POOL_ID} --filter "email=\\"${userEmail}\\""`;
+
+const users = JSON.parse((await execute(listUsers)).stdout);
+
+const sub = users.Users[0].Attributes.find((attr) => attr.Name === 'sub').Value;
+
+const sendPushNotification = `aws pinpoint get-user-endpoints --application-id ${process.env.PINPOINT_APPLICATION_ID} --user-id ${sub}`;
+
+console.log(
+  inspect(JSON.parse((await execute(sendPushNotification)).stdout), {
+    depth: null,
+  })
+);
