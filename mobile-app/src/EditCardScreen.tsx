15187311b5ee27e310755994f1d7b2495319f117
Dima Snisarenko <snisarenkodima@gmail.com>
2025-03-24 16:05:31 +0100

    feat(moa): manage tags on edit card screen

--- mobile-app/src/EditCardScreen.tsx
+++ mobile-app/src/EditCardScreen.tsx
@@ -1,11 +1,19 @@
 import { NavigationProp, Route } from '@react-navigation/native';
-import { CardItem } from '@vocably/model';
+import { CardItem, TagItem } from '@vocably/model';
 import React, { FC, useCallback, useEffect, useState } from 'react';
 import { Alert, ScrollView, StyleSheet, View } from 'react-native';
-import { Appbar, TextInput, useTheme } from 'react-native-paper';
+import {
+  ActivityIndicator,
+  Appbar,
+  Button,
+  Chip,
+  TextInput,
+  useTheme,
+} from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useSelectedDeck } from './languageDeck/useSelectedDeck';
 import { mainPadding } from './styles';
+import { TagsSelector } from './TagsSelector';
 
 const styles = StyleSheet.create({
   inputItem: {
@@ -24,13 +32,14 @@ type EditCardScreen = FC<{
 }>;
 
 export const EditCardScreen: EditCardScreen = ({ route, navigation }) => {
-  const { update } = useSelectedDeck({
+  const deck = useSelectedDeck({
     autoReload: false,
   });
 
   const { card } = route.params as EditCardParams;
   const [cardData, setCardData] = useState({ ...card.data });
   const [isUpdating, setIsUpdating] = useState(false);
+  const [savingTags, setSavingTags] = useState(false);
 
   const insets = useSafeAreaInsets();
 
@@ -50,15 +59,28 @@ export const EditCardScreen: EditCardScreen = ({ route, navigation }) => {
 
   const onUpdate = useCallback(async () => {
     setIsUpdating(true);
-    const updateResult = await update(card.id, cardData);
+    const updateResult = await deck.update(card.id, cardData);
     if (updateResult.success === false) {
       Alert.alert(
         'Error: Card update failed',
-        `Oops! Something went wrong while attempting to update the card. Please try again later.`
+        `Oops! Something went wrong while attempting to update the card. Please try again.`
       );
     }
     navigation.goBack();
-  }, [cardData, update, card]);
+  }, [cardData, deck.update, card]);
+
+  const onTagsChange = async (tags: TagItem[]) => {
+    cardData.tags = tags;
+    setSavingTags(true);
+    const updateResult = await deck.update(card.id, cardData);
+    setSavingTags(false);
+    if (updateResult.success === false) {
+      Alert.alert(
+        'Error: Card update failed',
+        `Oops! Something went wrong while attempting to update the card. Please try again.`
+      );
+    }
+  };
 
   const theme = useTheme();
   return (
@@ -91,6 +113,47 @@ export const EditCardScreen: EditCardScreen = ({ route, navigation }) => {
             padding: mainPadding,
           }}
         >
+          <View style={{ marginBottom: 24, alignItems: 'flex-start' }}>
+            <TagsSelector
+              value={cardData.tags}
+              onChange={onTagsChange}
+              deck={deck}
+              renderAnchor={({ openMenu, disabled }) => (
+                <Button onPress={openMenu} disabled={disabled} icon={'tag'}>
+                  Manage card tags (folders)
+                </Button>
+              )}
+            />
+            {(cardData.tags.length > 0 || savingTags) && (
+              <View
+                style={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'row',
+                  flexWrap: 'wrap',
+                  gap: 8,
+                  minHeight: 36,
+                }}
+              >
+                {cardData.tags.map((tag) => (
+                  <Chip
+                    key={tag.id}
+                    selectedColor={theme.colors.outlineVariant}
+                    mode="outlined"
+                    onClose={() =>
+                      onTagsChange(cardData.tags.filter((t) => t.id !== tag.id))
+                    }
+                  >
+                    {tag.data.title}
+                  </Chip>
+                ))}
+                {savingTags && (
+                  <ActivityIndicator color={theme.colors.onBackground} />
+                )}
+              </View>
+            )}
+          </View>
+
           <TextInput
             style={styles.inputItem}
             mode={'outlined'}
