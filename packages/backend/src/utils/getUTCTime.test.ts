6814bda4469d5ac58b157b0129a1166264da78f6
Dima Snisarenko <snisarenkodima@gmail.com>
2025-03-31 16:53:25 +0200

    chore: unit tests

--- packages/backend/src/utils/getUTCTime.test.ts
+++ packages/backend/src/utils/getUTCTime.test.ts
@@ -2,7 +2,7 @@ import { getUTCTime } from './getUTCTime';
 
 describe('getUTCTime', () => {
   it('works', () => {
-    expect(getUTCTime('08:00', 'Europe/Amsterdam')).toEqual('07:00');
+    expect(getUTCTime('08:00', 'Europe/Moscow')).toEqual('05:00');
     expect(getUTCTime('15:30', 'Asia/Kolkata')).toEqual('10:00');
     expect(getUTCTime('2:00', 'America/Bogota')).toEqual('07:00');
   });
