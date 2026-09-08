7faeb54059232bc88366ecb62ec74aadaa520ddc
Dima Snisarenko <snisarenkodima@gmail.com>
2026-09-08 17:47:58 +0200

    feat(extension): animated word highlighting on youtube

--- packages/extension-content-script/src/styles.ts
+++ packages/extension-content-script/src/styles.ts
@@ -9,13 +9,35 @@ export const setYouTubeStyles = () => {
   .vocably-word {
     cursor: pointer;
     display: inline-block;
-    border-radius: 8px;
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background-color: ${primary};
+      box-shadow: -2px 0 0 ${primary}, 2px 0 0 ${primary};
+      content: '';
+      border-radius: 8px;
+      z-index: -1;
+      opacity: 0;
+      transform: scale(50%);
+      transition: all 100ms ease-in-out;
+    }
+
+    &:hover {
+      color: white;
+
+      &::before {
+        opacity: 1;
+        transform: scale(100%);
+      }
+    }
   }
-  .vocably-word:hover {
-    background-color: ${primary};
-    box-shadow: -2px 0 0 ${primary}, 2px 0 0 ${primary};
-    color: white;
-  }
+
   .vocably-selectable-captions .ytp-caption-segment {
     transition: text-shadow ${youtubeHighlightDuration}ms ease-in-out;
   }
